2ae181a135690615d4fad40651c201e7cb40516b
Ronald Coe <ronald.coe@snowflake.com>
2022-11-10 18:32:03 -0500

    date formatting

--- streamlit_app.py
+++ streamlit_app.py
@@ -89,8 +89,10 @@ if len(t_sel) == 2:
   c2.subheader("Year Two: " + str(t_sel[1]))
   with c1:
     filt_c1 = (df_transactions['transactionYear'] == t_sel[0])
-    streamlit.table(df_transactions[filt_c1].sort_values(by='transactionDate', ascending=False))
+    df_transactions_f1 = df_transactions[filt_c1].sort_values(by='transactionDate', ascending=False)
+    streamlit.table(df_transactions_f1[['cv_transactionDate', 'transactionAmount']]))
 
   with c2:
     filt_c2 = (df_transactions['transactionYear'] == t_sel[1])
-    streamlit.table(df_transactions[filt_c2].sort_values(by='transactionDate', ascending=False))
+    df_transactions_f2 = df_transactions[filt_c2].sort_values(by='transactionDate', ascending=False)
+    streamlit.table(df_transactions_f2[['cv_transactionDate', 'transactionAmount']]))

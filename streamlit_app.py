eac4bfd8012e8689ab697e3d932ea636b18e93e7
Ronald Coe <ronald.coe@snowflake.com>
2022-11-11 11:16:56 -0500

    trying to print that table

--- streamlit_app.py
+++ streamlit_app.py
@@ -117,4 +117,4 @@ if len(t_sel) == 2:
   streamlit.table(df_combined_trans[['transactionMonth', 'Year_' + str(t_sel[0]), 'Year_' + str(t_sel[1])]])
 
   streamlit.area_chart(df_combined_trans, x= 'transactionMonth', y = [t_year1, t_year2])
-  df_combined_trans[['transactionMonth', 'Year_' + str(t_sel[0]), 'Year_' + str(t_sel[1])]].to_string(index=False)
+  streamlit.write(df_combined_trans[['transactionMonth', 'Year_' + str(t_sel[0]), 'Year_' + str(t_sel[1])]].to_string(index=False))

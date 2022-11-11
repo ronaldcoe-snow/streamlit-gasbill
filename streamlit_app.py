87010616263352533eb09b7ae5cbdbaeec0b4ddf
Ronald Coe <ronald.coe@snowflake.com>
2022-11-10 22:38:49 -0500

    setting decimal precision

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas as pd
 import snowflake.connector
 
-pd.option_context('display.float_format', '{:0.20f}'.format)
+pd.option_context('display.float_format', '{:0.2f}'.format)
   
 def get_demo_table_list():
   with my_cnx.cursor() as my_cur:
